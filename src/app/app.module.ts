26261bfb5edddf6c8ea42d4119c0486a8093ac65
C4tch0603 <e.miniberger@gmail.com>
2021-03-27 23:29:24 +0100

    Update to v0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    GalleryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
